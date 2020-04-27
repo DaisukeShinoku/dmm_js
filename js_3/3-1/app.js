e12692b302423bc19e071cf1b1a4f34ee701efa5
DaisukeShinoku <shinoku.daisuke.ycu@gmail.com>
2020-04-27 09:57:04 +0900

    [Start]js_3

---
+++ js_3/3-1/app.js
@@ -0,0 +1,10 @@
+var alertStrring;
+alertString = addString("WebCamp");
+
+alert(alertString);
+
+
+function addString(strA){
+	var addStr = "Hello" + strA;
+	return addStr;
+}
---
+++ js_3/3-2/app.js
@@ -0,0 +1,3 @@
+var promptStr = prompt('何か好きな文字を入力してみてください。');
+
+alert(promptStr);
